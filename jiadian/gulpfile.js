a53b6e4bfbcf53e0085ce7ab03fd919d5ff61006
John <601473400@qq.com>
2018-04-10 18:32:57 +0800

    jiadian,4-10

--- jiadian/gulpfile.js
+++ jiadian/gulpfile.js
@@ -1,25 +1,24 @@
 var gulp = require('gulp'),
     // sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer');
-    // minifycss = require('gulp-minify-css'),
+    minifycss = require('gulp-clean-css'),
     // jshint = require('gulp-jshint'),
-    // uglify = require('gulp-uglify'),
+    uglify = require('gulp-uglify'),
     // imagemin = require('gulp-imagemin'),
-    // rename = require('gulp-rename'),
+    rename = require('gulp-rename'),
     // concat = require('gulp-concat'),
-    // notify = require('gulp-notify'),
+    notify = require('gulp-notify'),
     // cache = require('gulp-cache'),
-    // del = require('del');
+    del = require('del');
 
 gulp.task('styles', function() {
   return gulp.src('css/index.css')
     // .pipe(sass({ style: 'expanded' }))
     .pipe(autoprefixer('last 2 version', 'ie 8'))
     .pipe(gulp.dest('css'))
-    console.log('ok');
-    // .pipe(rename({suffix: '.min'}))
-    // .pipe(minifycss())
-    // .pipe(gulp.dest('css'))
-    // .pipe(notify({ message: 'Styles task complete' }));
+    .pipe(rename({suffix: '.min'}))
+    .pipe(cleanCss())
+    .pipe(gulp.dest('css'))
+    .pipe(notify({ message: 'Styles task complete' }));
 });
 
--- jiadian/js/index.js
+++ jiadian/js/index.js
@@ -1,24 +1,16 @@
-var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer');
-    minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    // cache = require('gulp-cache'),
-    del = require('del');
 
-gulp.task('styles', function() {
-  return gulp.src('../css/index.css')
-    // .pipe(sass({ style: 'expanded' }))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'Firefox < 20'))
-    .pipe(gulp.dest('../css'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest('../css'))
-    .pipe(notify({ message: 'Styles task complete' }));
-});
+var 
+    prevBtn = $('#swiper-button-prev'),
+    nextBtn = $('#swiper-button-next'),
+    swiper = $('.swiper-container');
+
+// swiper前进后退按钮动画
+swiper.mouseover(function() {
+    prevBtn.css({'opacity':'1','left':'20px'});
+    nextBtn.css({'opacity':'1','right':'20px'});
+})
+swiper.mouseout(function() {
+    prevBtn.css({'opacity':'0','left':'0px'});
+    nextBtn.css({'opacity':'0','right':'0px'});
+})
 
