317551883e3c3f613690b5e6d58d6ce3d77318ef
John <601473400@qq.com>
2018-04-13 00:19:30 +0800

    4-13

--- jiadian/gulpfile.js
+++ jiadian/gulpfile.js
@@ -32,12 +32,17 @@ var gulp        = require('gulp'),
 	autoprefixer = require('gulp-autoprefixer'),
     minifyCss   = require('gulp-clean-css'),
     pump        = require('pump'),
+    imagemin    = require('gulp-imagemin'),
     changed      = require('gulp-changed'),
     sourcemaps   = require('gulp-sourcemaps'),
     uglify       = require('gulp-uglify'),
     runSequence  = require('run-sequence'),
-    rename 		= require('gulp-rename');
-
+    rename 		= require('gulp-rename'),
+    htmlmin 	= require('gulp-htmlmin'),
+    postcss 	= require('gulp-postcss'),
+    pngquant    = require('imagemin-pngquant'),
+    cache       = require('gulp-cache'),
+    browserSync  = require('browser-sync').create();
 
 // 清理目标目录
 gulp.task('clean', function(cb) {
@@ -46,17 +51,59 @@ gulp.task('clean', function(cb) {
         clean()
     ], cb)
 });
+// 同步更新浏览器
+gulp.task('browser', function() {
+    browserSync.init(
+        // 浏览器会监控以下目录，当以下目录中文件发生变化时，会同步更新
+        [
+            "./src/css/*.css"
+        ], {
+            // 定义服务器根目录
+            server: {
+                baseDir: "./src"
+            }
+        }
+    );
+});
+// 压缩图片
+// gulp.task('minImage', function (cb) {
+//     pump([
+//         gulp.src(config.src+'/img/homepage/*.{png,jpg,gif,ico}'),
+//         imagemin({
+//             optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+//             progressive: true,    //类型：Boolean 默认：false 无损压缩jpg图片
+//             interlaced: true,     //类型：Boolean 默认：false 隔行扫描gif进行渲染
+//             multipass: true       //类型：Boolean 默认：false 多次优化svg直到完全优化
+//         }),
+//         gulp.dest(config.dest+'/img/homepage/')
+//     ], cb);
+// });
+gulp.task('minImage', function () {
+    gulp.src(config.src+'/img/*.{png,jpg,gif,ico}')
+        .pipe(cache(imagemin({ //只压缩修改的图片
+            progressive: true, //无损压缩jpg图片
+            svgoPlugins: [{removeViewBox: false}], //不要移除svg的viewbox属性
+            use: [pngquant()] //使用pngquant深度压缩png图片的imagemin插件
+        })))
+        .pipe(gulp.dest(config.dest+'/img'));
+});
 // 压缩css,加前缀,重命名
 gulp.task('mincss', [], function(cb) {
     pump([
-        gulp.src(['src/css/*.css','!src/css/*.min.css']),
-        autoprefixer('last 2 version'),
+        gulp.src([config.src+'/**/*.css','!'+config.src+'/**/*.min.css']),
         rename({suffix: '.min'}),
+        changed(config.dest, { extension: '.css'}),
+        sourcemaps.init(),
+        postcss( autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: true
+        }) ),
         minifyCss(
             {keepSpecialComments: '*'}
             //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀        	
-        	),
-        gulp.dest('dist/css/')
+        ),
+        sourcemaps.write('.'),
+        gulp.dest(config.dest)
     ], cb
     )
 });
@@ -65,7 +112,7 @@ gulp.task('mincss', [], function(cb) {
 gulp.task('minjs', [], function(cb) {
     pump([
         // 获取原目录下所有的js文件
-        gulp.src(config.src + "/**/*.js"),
+        gulp.src([config.src + "/**/*.js",'!'+config.src+'/**/*.min.js']),
         // 执行更名操作
         rename({ suffix: '.min' }),
         // 每次打包时，只打包内容发生改变的文件
@@ -80,25 +127,49 @@ gulp.task('minjs', [], function(cb) {
         gulp.dest(config.dest)
     ], cb);
 });
+// 压缩HTML
+gulp.task('minhtml', [], function(cb) {
+
+    pump([
+        // 获取原目录下所有的html文件
+        gulp.src(config.src + "/*.html"),
+        // 每次打包时，只打包内容发生改变的文件
+        changed(config.dest, { extension:'.html' }),
+        // 执行html压缩
+        htmlmin({
+            removeComments: true,               // 清除HTML注释
+            collapseWhitespace: true,           // 压缩空格
+            collapseBooleanAttributes: true,    // 省略布尔属性的值 <input checked="true"/> => <input checked>
+            removeEmptyAttributes: true,        // 删除所有空格作属性值 <input id=""> => <input>
+            removeScriptTypeAttributes: true,   // 删除<script>的type="text/javascript"
+            removeStyleLinkTypeAttributes: true,// 删除<style>和<link>的type="text/css"
+            minifyJS: true,                     // 压缩页面JS
+            minifyCSS: true                     // 压缩页面CSS
+        }),
+        // 输出至目标目录
+        gulp.dest(config.dest)
+
+    ], cb);
+
+});
 // 搬运已压缩的css,js
 gulp.task('transCss',function() {
-	return gulp.src('src/css/*.min.css')
-		.pipe(gulp.dest('dist/css/'))
+	return gulp.src(config.src+'/**/*.min.css')
+		.pipe(gulp.dest(config.dest))
 })
 gulp.task('transJs',function() {
-	return gulp.src('src/js/*.min.js')
-		.pipe(gulp.dest('dist/js/'))
+	return gulp.src(config.src+'/**/*.min.js')
+		.pipe(gulp.dest(config.dest))
 })
 
 // 监听JS文件变改，即时调用任务执行JS增量打包
 gulp.task('watch', [], function(cb) {
-
+    gulp.watch(config.src + "/**/*.css", ['mincss']);
     gulp.watch(config.src + "/**/*.js", ['minjs']);
-    // gulp.watch(config.src + "/**/*.js", ['minjs']);
-
+    gulp.watch(config.src + "/*.html", ['minhtml']);
 });
 
 // 开始执行
 gulp.task('default', function(cb) {
-    runSequence('clean', 'minjs', 'watch', cb);
+    runSequence('clean','transCss','transJs', 'minjs','mincss', 'minhtml', 'watch', cb);
 });
