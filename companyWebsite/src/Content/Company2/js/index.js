03cdac9cb8e1bd2821c1a75dafface9eb738ae43
John <601473400@qq.com>
2018-05-08 00:02:52 +0800

    5-8

--- companyWebsite/src/Content/Company2/js/index.js
+++ companyWebsite/src/Content/Company2/js/index.js
@@ -1,6 +1,6 @@
-
+// 轮播  开始
 $('.carousel').carousel();
-
+// 右侧菜单的hover事件
 $(function () { 
     $(".ewm .b1").popover({
         html: true,
@@ -14,27 +14,9 @@ $(function () {
             return k_content2();
         }
     });
- }
+}
 );
-
-var W = document.documentElement.clientWidth;
-$(function() {
-    // 获取屏幕宽度,如果小屏, 证书部分删除两个
-
-    var _ctf = $('#ctf-main>div');
-    console.log(_ctf[0]);
-    if(W<=768) {
-        _ctf.filter(':first,:last').remove();
-
-
-        for(var i=0;i<navLi.length;i++) {
-            $(navLi[i]).on('click',function() {
-                navList.toggleClass('db');
-            })
-        };
-    }
-})
-
+// 右侧菜单的hover要显示的内容
 function k_content() {
     var data = $("<p><img class='ewm_i1' src='../Content/Company2/img/ewm_4.png'><span>广州市天河区黄埔大道中277号203</span></p><br><p><img src='../Content/Company2/img/ewm_5.png'><span>手机: +86-13929914698</span></p><br><p><img src='../Content/Company2/img/ewm_6.png'><span>邮件: service@jierutek.com</span></p>")
     return data;
@@ -44,6 +26,43 @@ function k_content2() {
     return data;
 }
 
+var W = $(window).width();
+var navLi = $('.nav-link');
+var navItem = $('.nav-item');
+console.log(W);
+$(function() {
+    // 获取屏幕宽度,如果小屏, 证书部分删除两个
+
+    var _ctf = $('#ctf-main>div');
+    // console.log(_ctf[0]);
+    if(W<=768) {
+        _ctf.filter(':first,:last').remove();
+
+        // 手机端点击导航栏的事件
+        for(var i=0;i<navLi.length;i++) {
+            navLi[i].index = i;
+
+            $(navLi[i]).on('click',function() {
+                // console.log(navLi[i]);
+                navItem.removeClass('activeItem-m');
+                navList.toggleClass('db');
+                for(var j=0;j<navLi.length;j++) {
+                    $(navLi[j]).removeClass('activeItem-m');
+                    $(navLi[this.index]).addClass('activeItem-m');
+                }
+            });
+        };
+        navItem.on('click',function() {
+            navList.toggleClass('db');
+            for (var k = 0; k < navLi.length; k++) {
+                $(navLi[k]).removeClass('activeItem-m');
+                navItem.addClass('activeItem-m');
+            }
+        })
+    }
+})
+
+
 
 // 手机端点击按钮, 导航栏出现
 var navBtn = $('.navbar-toggle');
@@ -52,8 +71,6 @@ navBtn.on('click',function () {
     // console.log(111);
     navList.toggleClass('db');
  })
-//  console.log(navList.children().children());
- var navLi = navList.children().children();
 
 
 
