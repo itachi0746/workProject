5aad4d0159c3e46103e59de742f6589297350439
John <601473400@qq.com>
2018-06-02 18:07:20 +0800

    官网修改

---
+++ companyWebsite/.history/gulpfile_20180510101041.js
@@ -0,0 +1,216 @@
+
+var config = {
+  src: "src/Content/Company2", // 文件资源css,js,image等
+  srcView: "src/View/Company2", // 文件资源html等
+  server: "src", // 服务器根目录
+  target: "View/Company2/index.html", // 要监视(执行)的html文件
+  dest: "dev/Content/Company2", // 生产环境文件夹
+  destView: "dev/View/Company2" // 生产环境文件夹
+};
+
+// 一次安装
+// 用cnpm安装文件夹会很多, 文件路径太深, 导致git上传不到
+// npm install pump gulp-sass gulp-clean gulp-autoprefixer gulp-clean-css gulp-imagemin gulp-changed gulp-sourcemaps gulp-uglify run-sequence gulp-rename gulp-htmlmin gulp-postcss imagemin-pngquant gulp-cache gulp-concat del browser-sync --save-dev
+var gulp        = require('gulp'),
+	clean        = require('gulp-clean'),
+	autoprefixer = require('gulp-autoprefixer'),
+    minifyCss   = require('gulp-clean-css'),
+    pump        = require('pump'),
+    imagemin    = require('gulp-imagemin'),
+    changed      = require('gulp-changed'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    uglify       = require('gulp-uglify'),
+    runSequence  = require('run-sequence'),
+    rename 		= require('gulp-rename'),
+    htmlmin 	= require('gulp-htmlmin'),
+    postcss 	= require('gulp-postcss'),
+    pngquant    = require('imagemin-pngquant'),
+    cache       = require('gulp-cache'),
+    sass       = require('gulp-sass'),
+    concat       = require('gulp-concat'),
+    del       = require('del'),
+    browserSync  = require('browser-sync').create();
+
+// 清理目标目录
+gulp.task('clean', function(cb) {
+    pump([
+        gulp.src(config.dest),
+        clean()
+    ], cb);
+});
+// 同步更新浏览器
+// gulp.task('browser', function() {
+//     browserSync.init(
+//         // 浏览器会监控以下目录，当以下目录中文件发生变化时，会同步更新
+//         [
+//             "./dev/css/*.css"
+//         ], {
+//             // 定义服务器根目录
+//             server: {
+//                 baseDir: "./dev"
+//             }
+//         }
+//     );
+// });
+gulp.task('browser', function () {
+    browserSync.init({
+        files: ['**'],  // 修改HTML也刷新
+        server: {
+            baseDir: config.server,  // 设置服务器的根目录
+            index: config.target // 指定默认打开的文件
+        },
+        port: 8050  // 指定访问服务器的端口号
+    });
+    
+});
+// 压缩图片
+// gulp.task('minImage', function (cb) {
+//     pump([
+//         gulp.src(config.src+'/img/homepage/*.{png,jpg,gif,ico}'),
+//         imagemin({
+//             optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+//             progressive: true,    //类型：Boolean 默认：false 无损压缩jpg图片
+//             interlaced: true,     //类型：Boolean 默认：false 隔行扫描gif进行渲染
+//             multipass: true       //类型：Boolean 默认：false 多次优化svg直到完全优化
+//         }),
+//         gulp.dest(config.dest+'/img/homepage/')
+//     ], cb);
+// });
+gulp.task('minImage', function () {
+    gulp.src(config.src+'/img/*.{png,jpg,gif,ico}')
+        .pipe(cache(imagemin({ //只压缩修改的图片
+            progressive: true, //无损压缩jpg图片
+            svgoPlugins: [{removeViewBox: false}], //不要移除svg的viewbox属性
+            use: [pngquant()] //使用pngquant深度压缩png图片的imagemin插件
+        })))
+        .pipe(gulp.dest(config.dest+'/img'));
+});
+// 编译sass
+gulp.task('sassToCss', function(cb){
+    pump([
+        gulp.src(config.src+'/scss/*.scss'),
+        sass(),
+        gulp.dest(config.src+'/css/')
+    ]);
+  });
+// 编译,合并,重命名,加前缀,压缩
+gulp.task('mincss', [], function(cb) {
+    pump([
+        gulp.src(config.src + '/scss/*.scss'),
+        sass(),
+        // concat('index.css'),  //合并后的文件名
+        // rename({suffix: '.min'}),
+        changed(config.src, { extension: '.css'}),
+        sourcemaps.init(),
+        postcss( autoprefixer({
+            // 兼容主流浏览器的最新两个版本
+            browsers: ['last 2 versions'],
+            cascade: true
+        }) ),
+        // minifyCss(
+        //     {keepSpecialComments: '*'}
+        //     //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀        	
+        // ),
+        sourcemaps.write('.'),
+        gulp.dest(config.src + '/css/')
+    ], cb
+    );
+});
+// gulp.task('mincss', [], function(cb) {
+//     pump([
+//         gulp.src([config.src+'/**/*.css','!'+config.src+'/**/*.min.css']),
+//         rename({suffix: '.min'}),
+//         changed(config.dest, { extension: '.css'}),
+//         sourcemaps.init(),
+//         postcss( autoprefixer({
+//             browsers: ['last 2 versions'],
+//             cascade: true
+//         }) ),
+//         minifyCss(
+//             {keepSpecialComments: '*'}
+//             //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀        	
+//         ),
+//         sourcemaps.write('.'),
+//         gulp.dest(config.dest)
+//     ], cb
+//     )
+// });
+
+ // 执行JS压缩
+gulp.task('minjs', [], function(cb) {
+    pump([
+        // 获取原目录下所有的js文件
+        gulp.src(config.src + "/js/*.js"),
+        // 执行更名操作
+        rename({ suffix: '.min' }),
+        // 每次打包时，只打包内容发生改变的文件
+        changed(config.dest, { extension:'.js' }),
+        // 生成sourcemap，需要配合后面的sourcemaps.write()
+        sourcemaps.init(),
+        // 执行JS压缩
+        uglify(),
+        // 生成sourcemap
+        sourcemaps.write(),
+        // 输出至目标目录
+        gulp.dest(config.dest + '/js/')
+    ], cb);
+});
+// 压缩HTML
+gulp.task('minhtml', [], function(cb) {
+
+    pump([
+        // 获取原目录下所有的html文件
+        gulp.src(config.srcView + "/*.html"),
+        // 每次打包时，只打包内容发生改变的文件
+        changed(config.dest, { extension:'.html' }),
+        // 执行html压缩
+        htmlmin({
+            removeComments: true,               // 清除HTML注释
+            collapseWhitespace: true,           // 压缩空格
+            collapseBooleanAttributes: true,    // 省略布尔属性的值 <input checked="true"/> => <input checked>
+            removeEmptyAttributes: true,        // 删除所有空格作属性值 <input id=""> => <input>
+            removeScriptTypeAttributes: true,   // 删除<script>的type="text/javascript"
+            removeStyleLinkTypeAttributes: true,// 删除<style>和<link>的type="text/css"
+            minifyJS: true,                     // 压缩页面JS
+            minifyCSS: true                     // 压缩页面CSS
+        }),
+        // 输出至目标目录
+        gulp.dest(config.destView)
+
+    ], cb);
+
+});
+// 搬运插件
+gulp.task('transLibs',function() {
+	return gulp.src(config.src+'/libs/*.*')
+        .pipe(gulp.dest(config.dest+'/libs/'));
+});
+gulp.task('transFont',function() {
+	return gulp.src(config.src+'/font/*.*')
+        .pipe(gulp.dest(config.dest+'/font/'));
+});
+
+// 监听文件变改，即时调用任务执行增量打包
+gulp.task('watch', [], function(cb) {
+    gulp.watch([config.src + "/scss/*.scss",config.src + '/styles/*.scss'], ['mincss']);
+    // gulp.watch(config.src + "/js/*.js", ['minjs']);
+    // gulp.watch(config.src + "/*.html", ['minhtml']);
+    // var imgWatcher = gulp.watch(config.src + "/img/*.{png,jpg,gif,ico}", ['minImage']);
+
+    // 监听文件删除事件,图片同步删除~~
+    // imgWatcher.on('change', function(event) {
+    //     if(event.type === 'deleted') {
+    //         del(config.dest + event.path.basename(event.path, [png,jpg,gif,ico]), [png,jpg,gif,ico]);
+    //     }
+    // });
+});
+
+// 开始打包全部文件
+// gulp.task('default', function(cb) {
+//     runSequence('clean', 'minImage', 'transLibs', 'transFont', 'minjs', 'mincss', 'minhtml', cb);
+// });
+
+// 只执行编译sass和同步浏览器
+gulp.task('default', function(cb) {
+    runSequence('mincss','browser', 'watch', cb);
+});
---
+++ companyWebsite/.history/src/Content/Company2/js/index_20180602172248.js
@@ -0,0 +1,484 @@
+// 轮播  开始
+$(".carousel").carousel();
+// 右侧菜单的hover事件
+$(function() {
+  $(".ewm .b1").popover({
+    html: true,
+    content: function() {
+      return k_content();
+    }
+  });
+  $(".ewm .b2").popover({
+    html: true,
+    content: function() {
+      return k_content2();
+    }
+  });
+  honerPart();
+
+  $(".ewm .b1").hover(
+    function() {
+      $(this).addClass("activeItem");
+      $(this)
+        .find("img")
+        .get(0).src =
+        "../../Content/Company2/img/ewm_1_hover.png";
+    },
+    function() {
+      $(this).removeClass("activeItem");
+      $(this)
+        .find("img")
+        .get(0).src =
+        "../../Content/Company2/img/ewm_1.png";
+    }
+  );
+  $(".ewm .b2").hover(
+    function() {
+      $(this).addClass("activeItem");
+      $(this)
+        .find("img")
+        .get(0).src =
+        "../../Content/Company2/img/ewm_2_hover.png";
+    },
+    function() {
+      $(this).removeClass("activeItem");
+      $(this)
+        .find("img")
+        .get(0).src =
+        "../../Content/Company2/img/ewm_2.png";
+    }
+  );
+  $(".ewm .b3").hover(
+    function() {
+      $(this).addClass("activeItem");
+    },
+    function() {
+      $(this).removeClass("activeItem");
+    }
+  );
+  // 服务范围的hover
+  $(".features div").hover(
+    function() {
+      $(this).find(".f-before").addClass("dn");
+      $(this).find(".f-after").addClass("db");
+      $(this).find("h4").css('color','#f88929');
+      $(this).css({
+        'box-shadow':'0px 0px 5px #888888',
+        '-webkit-box-shadow':'0px 0px 5px #888888',
+        '-moz-box-shadow':'0px 0px 5px #888888',
+        '-o-box-shadow':'0px 0px 5px #888888',
+        '-ms-box-shadow':'0px 0px 5px #888888'
+      });
+    },
+    function() {
+      $(this).find(".f-before").removeClass("dn");
+      $(this).find(".f-after").removeClass("db");
+      $(this).find("h4").css('color','#7c7c7c');
+      $(this).css({
+        'box-shadow':'none',
+        '-webkit-box-shadow':'none',
+        '-moz-box-shadow':'none',
+        '-o-box-shadow':'none',
+        '-ms-box-shadow':'none'
+      });
+    }
+  );
+  // 华为云的hover
+  $('#huaweiyun .h-item').hover(
+    function () {  
+      $(this).find('.h-before').addClass('dn');
+      $(this).find(".h-after").addClass("dib");
+      $(this).find("p").css('color','#f88929');
+
+    },
+    function () {  
+      $(this).find('.h-before').removeClass('dn');
+      $(this).find(".h-after").removeClass("dib");
+      $(this).find("p").css('color','#000');
+
+    }
+  );
+  // 我们的产品的hover
+  $('#OurProducts .op-box').hover(
+    function () {  
+      $(this).find('.op-before').addClass('dn');
+      $(this).find(".op-after").addClass("dib");
+      $(this).find("h4").css('color','#f88929');
+      $(this).css({
+        'box-shadow':'0px 0px 5px #888888',
+        '-webkit-box-shadow':'0px 0px 5px #888888',
+        '-moz-box-shadow':'0px 0px 5px #888888',
+        '-o-box-shadow':'0px 0px 5px #888888',
+        '-ms-box-shadow':'0px 0px 5px #888888',
+        '-ms-transform':'scale(1.05)',
+        '-moz-transform':'scale(1.05)',
+        '-webkit-transform':'scale(1.05)',
+        '-o-transform':'scale(1.05)',
+        'transform':'scale(1.05)'
+      });
+    },
+    function () { 
+      $(this).find('.op-before').removeClass('dn');
+      $(this).find(".op-after").removeClass("dib");
+      $(this).find("h4").css('color','#7c7c7c');
+      $(this).css({
+        'box-shadow':'none',
+        '-webkit-box-shadow':'none',
+        '-moz-box-shadow':'none',
+        '-o-box-shadow':'none',
+        '-ms-box-shadow':'none',
+        '-ms-transform':'scale(1.0)',
+        '-moz-transform':'scale(1.0)',
+        '-webkit-transform':'scale(1.0)',
+        '-o-transform':'scale(1.0)',
+        'transform':'scale(1.0)'
+      });
+    }
+  );
+
+});
+
+// ============================================
+// 获取header高度
+var headerH = $("header").height();
+// 获取屏幕宽度
+var W = $(window).width();
+var navLi = $(".nav-link");
+
+// 导航栏-资质荣誉
+var linkH = $('[href="#honer"]');
+// 不是资质荣誉的部分
+var notHoner = $("section.box");
+// 资质荣誉的部分
+var isHoner = $("#honer");
+// console.log(W);
+
+$(function() {
+  // 如果小屏, 证书部分删除两个
+
+  // 如果是手机
+  if (W <= 768) {
+    // 删两个证书
+    ctfGroup
+      .children()
+      .filter(":first,:last")
+      .remove();
+    ctfGroup2
+      .children()
+      .filter(":first,:last")
+      .remove();
+    // 导航的首页加个active类
+    $(navLi[0]).addClass("activeItem-m");
+    // 手机端点击导航栏的事件
+    for (var i = 0; i < navLi.length; i++) {
+      navLi[i].index = i;
+
+      $(navLi[i]).on("click", function() {
+        honerList.css("display", "none");
+        // console.log(navLi[i]);
+        // navItem.removeClass('activeItem-m');
+        // 导航栏消失
+        navList.toggleClass("db");
+        for (var j = 0; j < navLi.length; j++) {
+          $(navLi[j]).removeClass("activeItem-m");
+          $(navLi[this.index]).addClass("activeItem-m");
+        }
+
+        if (this.index === 6) {
+          // 导航栏-资质荣誉的点击,其他消失,只显示资质荣誉部分
+          for (var k = 0; k < notHoner.length; k++) {
+            $(notHoner[k]).css("display", "none");
+          }
+          isHoner.css("display", "block");
+        } else {
+          // 其他的点击
+          for (var k = 0; k < notHoner.length; k++) {
+            $(notHoner[k]).css("display", "block");
+          }
+          isHoner.css("display", "none");
+        }
+      });
+    }
+  }
+  // 如果是电脑
+  else {
+    // 导航的首页加个active类
+    $(navLi[0]).addClass("activeItem");
+
+    // for(var j=0;j<navLi.length;j++) {
+    //     $(navLi[j]).hover(
+    //         function () {
+    //             this.addClass('nav-link-hover');
+    //         },
+    //         function () {
+    //             this.removeClass('nav-link-hover');
+    //         }
+    //     )
+    // };
+
+    for (var i = 0; i < navLi.length; i++) {
+      navLi[i].index = i;
+
+      // 导航的hover事件
+      $(navLi[i]).hover(
+        function() {
+          $(this).addClass("nav-link-hover");
+        },
+        function() {
+          $(this).removeClass("nav-link-hover");
+        }
+      );
+
+      $(navLi[i]).on("click", function() {
+        if (this.index === 6) {
+          // 导航栏-资质荣誉的点击,其他消失,只显示资质荣誉部分
+          for (var k = 0; k < notHoner.length; k++) {
+            $(notHoner[k]).css("display", "none");
+          }
+          isHoner.css("display", "block");
+        } else {
+          // 其他的点击
+          for (var k = 0; k < notHoner.length; k++) {
+            $(notHoner[k]).css("display", "block");
+          }
+          isHoner.css("display", "none");
+        }
+      });
+    }
+  }
+
+  // 导航栏-资质荣誉的点击
+  linkH.on("click", function() {
+    for (var k = 0; k < notHoner.length; k++) {
+      $(notHoner[k]).css("display", "none");
+    }
+    isHoner.css("display", "block");
+  });
+});
+// 右侧菜单的hover要显示的内容
+function k_content() {
+  var data = $(
+    "<p><img class='ewm_i1' src='../../Content/Company2/img/ewm_4.png'><span>广州市天河区黄埔大道中277号203</span></p><br><p><img src='../../Content/Company2/img/ewm_5.png'><span>手机: +86-13929914698</span></p><br><p><img src='../../Content/Company2/img/ewm_6.png'><span>邮件: service@jierutek.com</span></p>"
+  );
+  return data;
+}
+function k_content2() {
+  var data = $(
+    "<img class='' src='../../Content/Company2/img/lianxiwomenerweima.png'>"
+  );
+  return data;
+}
+
+// 手机端点击按钮, 导航栏出现
+var navBtn = $(".navbar-toggle");
+var navList = $(".navbar-collapse");
+navBtn.on("click", function() {
+  // console.log(111);
+  navList.toggleClass("db");
+});
+
+var ctfBtn = $(".btn.icon-btn");
+var ctfBtn2 = $(".btn.icon-btn2");
+var ctfMain = $(".rowWp");
+var ctfGroup = $("#row1");
+var ctfGroup2 = $("#row2");
+// ctfGroup2.html(ctfGroup.html());
+// console.log(ctfBtn,ctfBtn2);
+// 距离
+var dis = 0;
+
+// 点击移动证书
+ctfBtn.on("click", function() {
+  if (dis <= -ctfGroup.width()) {
+    return;
+  } else {
+    ctfBtn2.removeClass("disabled");
+
+    var itemW = $(ctfGroup.children()).width();
+    var itemP = $(ctfGroup.children()).css("padding-left");
+    dis -= itemW + parseInt(itemP) * 2;
+    // console.log(dis);
+
+    ctfMain.css("left", dis);
+
+    if (dis <= -ctfGroup.width()) {
+      $(this).addClass("disabled");
+    }
+  }
+});
+ctfBtn2.on("click", function() {
+  if (dis >= 0) {
+    return;
+  } else {
+    ctfBtn.removeClass("disabled");
+    var itemW = $(ctfGroup.children()).width();
+    var itemP = $(ctfGroup.children()).css("padding-left");
+    dis += itemW + parseInt(itemP) * 2;
+    console.log(dis);
+
+    ctfMain.css("left", dis);
+    if (dis >= 0) {
+      $(this).addClass("disabled");
+    }
+  }
+});
+
+// 证书的点击
+var ctfOne = ctfGroup.children();
+var ctfTwo = ctfGroup2.children();
+// 证书特写
+var ctfBig = $(".ctf-big");
+
+for (var i = 0; i < ctfOne.length; i++) {
+  ctfOne[i].index = i;
+  $(ctfOne[i]).on("click", function() {
+    // 小图src
+    var _targetSrc = $(ctfOne[this.index])
+      .find(".img-responsive")
+      .get(0).src;
+    // 文字
+    var temp = $(ctfOne[this.index])
+      .find("p")
+      .html();
+    ctfBig.find("p").html(temp);
+
+    // 大图src
+    // var _targetBigSrc = ctfBig.find('.img-responsive').get(0).src;
+    // 其他消失,只显示资质荣誉部分
+    for (var k = 0; k < notHoner.length; k++) {
+      $(notHoner[k]).css("display", "none");
+    }
+    isHoner.css("display", "block");
+    ctfBig.find(".img-responsive").get(0).src = _targetSrc;
+
+    ctfBig.css("display", "block");
+    window.scrollTo(0, headerH);
+  });
+
+  //     ctfTwo[i].index = i;
+  //     $(ctfTwo[i]).on('click',function() {
+  //         window.location = './View/honer.html';
+  //     });
+}
+for (var i = 0; i < ctfTwo.length; i++) {
+  ctfTwo[i].index = i;
+  $(ctfTwo[i]).on("click", function() {
+    // 小图src
+    var _targetSrc = $(ctfTwo[this.index])
+      .find(".img-responsive")
+      .get(0).src;
+    // 文字
+    var temp = $(ctfTwo[this.index])
+      .find("p")
+      .html();
+    ctfBig.find("p").html(temp);
+
+    // 大图src
+    // var _targetBigSrc = ctfBig.find('.img-responsive').get(0).src;
+    // 其他消失,只显示资质荣誉部分
+    for (var k = 0; k < notHoner.length; k++) {
+      $(notHoner[k]).css("display", "none");
+    }
+    isHoner.css("display", "block");
+    ctfBig.find(".img-responsive").get(0).src = _targetSrc;
+
+    ctfBig.css("display", "block");
+    window.scrollTo(0, headerH);
+  });
+
+  //     ctfTwo[i].index = i;
+  //     $(ctfTwo[i]).on('click',function() {
+  //         window.location = './View/honer.html';
+  //     });
+}
+var ctfZlList = [
+  "程序分布式运行方法、装置及系统",
+  "分布式系统的资源操作方法及装置、分布式系统"
+];
+
+var ctfRzList = [
+  "接入信息义齿管理系统",
+  "伊社制造业企业资源管理系统",
+  "企商云惠鲜花批发与花店管理云平台",
+  "企商云惠客户关系管理云平台",
+  "企商云惠微信商城云平台",
+  "企商云惠餐饮管理云平台",
+  "企商云惠微信俱乐部云平台",
+  "接入信息工程项目管理系统",
+  "伊社餐厅管理系统",
+  "接入信息月饼管理系统"
+];
+
+var ctfGxList = ["高新技术企业", "高新技术企业"];
+
+var honerList = $(".ctf-list");
+// var ctfBig = $('.row.ctf-big');
+
+function honerPart() {
+  var ctfListWp = $(".ctf-list-wp");
+
+  var ctfZl = $("#ctf-zl").children();
+  var ctfRz = $("#ctf-rz").children();
+  var ctfGx = $("#ctf-gx").children();
+
+  // console.log(ctfList);
+  // ========点击放大证书=================
+  biggerCtf(ctfZl, ctfZlList);
+  biggerCtf(ctfRz, ctfRzList);
+  biggerCtf(ctfGx, ctfGxList);
+
+
+  // ==========点击按钮显示对应部分============
+  var honerBtn = $(".honer button");
+  // console.log(honerList);
+  for (var j = 0; j < honerBtn.length; j++) {
+    // 这个自定义属性不用转对象
+    honerBtn[j].index = j;
+    // console.log()
+    $(honerBtn[j]).on("click", function() {
+      // 按钮颜色
+      for (var z = 0; z < notHoner.length; z++) {
+        $(honerBtn[z]).css({ color: "#2f77d0", background: "#fff" });
+      }
+      $(this).css({ color: "#fff", background: "#2f77d0" });
+
+      // 隐藏大图
+      ctfBig.css("display", "none");
+      // 显示证书列表
+      ctfListWp.css("display", "block");
+
+      for (var k = 0; k < honerList.length; k++) {
+        $(honerList[k]).css("display", "none");
+      }
+      $(honerList[this.index]).css("display", "block");
+      // console.log(honerList[this.index]);
+    });
+  }
+}
+
+function biggerCtf(tgtList, tgtNameList) {
+  // ========点击放大证书=================
+  for (var i = 0; i < tgtList.length; i++) {
+    tgtList[i].index = i;
+    // console.log(tgtList.length);
+
+    $(tgtList[i]).on("click", function() {
+      honerList.css("display", "none");
+      // 小图src
+      var temp = $(tgtList[this.index]);
+
+      // console.log(temp2.find(".img-responsive"));
+      temp = temp.find(".img-responsive").get(0) || temp.find(".bigger-img").get(0);
+      
+      var _targetSrc = temp.src;
+      // 大图src
+      // var _targetBigSrc = ctfBig.find('.img-responsive').get(0).src;
+      ctfBig.find(".img-responsive").get(0).src = _targetSrc;
+      ctfBig.find("p").html(tgtNameList[this.index]);
+
+      ctfBig.css("display", "block");
+      console.log(headerH);
+      window.scrollTo(0, headerH);
+    });
+  }
+}
