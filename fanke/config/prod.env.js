3e983891b467d3b9e14d04d6c9a91d5173be5fd2
John <601473400@qq.com>
2018-08-07 18:53:00 +0800

    77

--- fanke/config/prod.env.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  BASE_URL:"'/api'"
-  // BASE_URL:"'http://localhost:8080'"
-
-}
--- fanke/src/eventBus/eventBus.js
+++
@@ -1,4 +0,0 @@
-
-// 全局事件
-import Vue from 'vue';
-export const EventBus = new Vue();
